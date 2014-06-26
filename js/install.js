d01c0548fad44500f4998490f357ff0833474f9d
Guillaume Giraud <guillaume.giraud@openmailbox.org>
2014-06-26 19:13:50 +0200

    m

--- js/install.js
+++ js/install.js
@@ -1,25 +1,26 @@
-var button = document.querySelector('button#install'),
+var button = document.createElement('BUTTON'),
     origin = window.location.origin,
     request = navigator.mozApps.checkInstalled(origin + "/manifest.webapp");
 
+
 request.onsuccess = function() {
     if (this.result) {
-        button.textContent = "update app";
         console.log('Application is already installed');
     } else {
-        button.textContent = "install app";
         console.log('Application is not installed');
-    }
 
-    button.addEventListener('click', function(e) {
-        var request = window.navigator.mozApps.install(origin + "/manifest.webapp");
-        request.onsuccess = function() {
-            alert("Installation successful");
-        };
-        request.onerror = function(err) {
-            alert("Install failed, error: " + this.error.name);
-        };
-    }, false);
+        document.body.appendChild(button);
+		button.textContent = "Install App"
+        button.addEventListener('click', function(e) {
+            var request = window.navigator.mozApps.install(origin + "/manifest.webapp");
+            request.onsuccess = function() {
+                alert("Installation successful");
+            };
+            request.onerror = function(err) {
+                alert("Install failed, error: " + this.error.name);
+            };
+        }, false);
+    }
 };
 
 request.onerror = function() {
