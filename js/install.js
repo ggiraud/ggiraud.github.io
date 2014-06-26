302816e2255d43b928fec99a8b2a14d199642028
Guillaume Giraud <guillaume.giraud@openmailbox.org>
2014-06-26 18:35:17 +0200

    m

--- js/install.js
+++ js/install.js
@@ -1,21 +1,23 @@
 var button = document.querySelector('button#install'),
-	origin = window.location.origin;
+    origin = window.location.origin;
 
-button.addEventListener('click', function(e) {
-    new Promise(function(resolve, reject) {
-        var request = navigator.mozApps.checkInstalled(origin + "/manifest.webapp");
-        request.onsuccess = function() {
-            if (this.result) {
-                reject(new Error('Application is already installed'));
-            } else {
-            	resolve('Application is not installed');
-            }
-        };
-        request.onerror = function() {
-        	reject(this.error);
-        };
-    }).then(function() {
-        return new Promise(function(resolve, reject) {
+new Promise(function(resolve, reject) {
+    var request = navigator.mozApps.checkInstalled(origin + "/manifest.webapp");
+    request.onsuccess = function() {
+        if (this.result) {
+            button.textContent = "update app";
+            resolve('Application is already installed');
+        } else {
+            button.textContent = "install app";
+            resolve('Application is not installed');
+        }
+    };
+    request.onerror = function() {
+        reject(this.error);
+    };
+}).then(function() {
+    button.addEventListener('click', function(e) {
+        new Promise(function(resolve, reject) {
             var request = window.navigator.mozApps.install(origin + "/manifest.webapp");
             request.onsuccess = function() {
                 resolve("Installation successful");
@@ -23,10 +25,12 @@ button.addEventListener('click', function(e) {
             request.onerror = function(err) {
                 reject(new Error("Install failed, error: " + this.error.name));
             };
+        }).then(function(msg) {
+            alert(msg);
+        }, function(err) {
+            alert(err.message);
         });
-    }).then(function(msg) {
-        alert(msg);
-    }).catch (function(err) {
-        alert(err.message);
-    });
-}, false);
+    }, false);
+}).catch(function(err) {
+	alert(err.msg);
+});
