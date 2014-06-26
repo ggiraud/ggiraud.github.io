27df808d6d66a87c62806dd74c844921ef0248f2
Guillaume Giraud <guillaume.giraud@openmailbox.org>
2014-06-26 17:42:33 +0200

    m

--- js/install.js
+++ js/install.js
@@ -1,11 +1,27 @@
 var button = document.querySelector('button#install');
 
 button.addEventListener('click', function(e) {
-	var request = window.navigator.mozApps.install("http://ggiraud.github.io/manifest.webapp");
-	request.onsuccess = function() {
-		alert("Installation successful");
-	};
-	request.onerror = function() {
-		alert("Install failed, error: " + this.error.name);
-	};
+    new Promise(function(resolve, reject) {
+        var request = navigator.mozApps.checkInstalled("http://ggiraud.github.io/manifest.webapp");
+        request.onsuccess = function() {
+            reject(new Error('application already installed'));
+        };
+        request.onerror = function() {
+            resolve('application not installed');
+        };
+    }).then(function() {
+        var request = window.navigator.mozApps.install("http://ggiraud.github.io/manifest.webapp");
+        return new Promise(function(resolve, reject) {
+            request.onsuccess = function() {
+                resolve("Installation successful");
+            };
+            request.onerror = function(err) {
+                reject(new Error("Install failed, error: " + this.error.name));
+            };
+        });
+    }).then(function(msg) {
+        alert(msg);
+    }).catch (function(msg) {
+        alert(msg);
+    });
 }, false);
