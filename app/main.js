258e0ecbe3ad24e956046166f459a2700ac188bc
ggiraud <guillaume.giraud@openmailbox.org>
2014-09-12 22:29:37 +0200

    m

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 require.config({
     paths: {
+        "hello": "/bower_components/hello/dist/hello.all.js",
         "es6-promise": "/bower_components/es6-promise/promise.min",
         "jquery": "/bower_components/jquery/dist/jquery.min",
         "bacon": "/bower_components/bacon/dist/Bacon.min",
@@ -8,9 +9,12 @@ require.config({
         "rv": "/bower_components/requirejs-ractive/rv",
         "localforage": "/bower_components/localforage/dist/localforage.min",
         "restyle": "/bower_components/restyle/build/restyle",
-        "underscore": '/bower_components/underscore/underscore-min'
+        "underscore": "/bower_components/underscore/underscore-min"
     },
     shim: {
+        "hello": {
+            exports: "hello"
+        },
         "es6-promise": {
             exports: "Promise"
         },
@@ -26,23 +30,13 @@ require.config({
     }
 });
 
-require(['search/search', 'git/git'], function(search, git) {
+require(['search/search', 'hello'], function(search, hello) {
     var button = document.createElement('BUTTON');
     button.textContent = "github";
     document.body.appendChild(button);
 
-    var script = document.createElement('SCRIPT');
-    script.setAttribute('src', 'https://github.com/login/oauth/authorize?client_id=c1882e1253b6b429c0f4&callback=jsonpCallback');
-    script.setAttribute('type', 'text/javascript');
-
     button.onclick = function(e) {
-        document.head.appendChild(script);
-        document.head.removeChild(script);
+
     };
 
-    function jsonPCallback(val) {
-        console.log(val);
-    }
-
-    // git.authorize();
 });
