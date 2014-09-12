5fee707c27a7adf3343593eae5f82f2192fecf34
ggiraud <guillaume.giraud@openmailbox.org>
2014-09-12 22:59:21 +0200

    m

--- app/main.js
+++ app/main.js
@@ -35,21 +35,46 @@ require(['search/search', 'hello'], function(search, hello) {
     button.textContent = "github";
     document.body.appendChild(button);
 
-    hello.init({
-        github: 'c1882e1253b6b429c0f4'
-    },{
-        redirect_uri: "http://ggiraud.github.io",
-        response_type: "code"
-    });
+    // hello.init({
+    //     github: 'c1882e1253b6b429c0f4'
+    // },{
+    //     redirect_uri: "http://ggiraud.github.io",
+    //     response_type: "code"
+    // });
 
-    hello.on('auth.login', function(auth) {
-        console.log(auth);
-    });
+    // hello.on('auth.login', function(auth) {
+    //     console.log(auth);
+    // });
 
-    button.onclick = function(e) {
-        hello('github').login().then(function() {
-            console.log(hello('github').getAuthResponse());
+    // button.onclick = function(e) {
+    //     hello('github').login().then(function() {
+    //         console.log(hello('github').getAuthResponse());
+    //     });
+    // };
+
+    var github = document.createElement('DIV');
+
+    hello.on('auth.login', function(r){
+        // Get Profile
+        console.log("auth.login");
+        hello.api(r.network+':/me', function(p){
+            document.getElementById(r.network).innerHTML = "<img src='"+ p.thumbnail + "' width=24/>Connected to "+ r.network+" as " + p.name;
         });
-    };
+        hello.api(r.network+':/user/repos', function(r){
+            console.log(r);
+            for(var i=0;i<r.length;i++){
+                var li = document.createElement('li');
+                var a = document.createElement('a');
+                a.innerHTML = r[i].full_name;
+                a.href=r[i].html_url;
+                li.appendChild(a);
+                document.getElementById('result').appendChild(li);
+            }
+        });
+    });
+
+    hello.init({
+        github : 'c1882e1253b6b429c0f4'
+    },  {redirect_uri:'../index.html'});
 
 });
